9ba863938a02599d8a18aedc13650222f64fc638
TracyAntonia <aantonjuguna@gmail.com>
2023-08-24 08:01:00 +0300

    completed assignments

--- lib/person.py
+++ lib/person.py
@@ -4,4 +4,15 @@ class Person:
     # Class body goes here
 
     #Instance method definition
+    def talk(sef):
+        print("Hello World!")
+
+    def walk(sef):
+        print("The person is walking.")    
     pass
+
+scott = Person()
+scott.talk()
+
+malia = Person()
+malia.walk()
